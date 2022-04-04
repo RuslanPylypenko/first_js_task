b805ed0dd07fb93d01fa0a226db536da865e9293
Ruslan <41823506+RuslanPylypenko@users.noreply.github.com>
2022-04-04 10:48:13 +0300

    update

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const chooseOptimalDistance = (t, k, ls) => {
-    const distances = ls.sort((a, b) => b - a);
-
-    if (k > distances.length) return null;
-    if (distances.slice(-k).reduce((acc, item) => acc + item) > t) return null
-
-    let optimal = 0;
-
-    for (let i = 0; i < distances.length; i++) {
-        let res = distances[i];
-        if (distances[i] > t) continue;
-
-        if (k === 1) return distances[i];
-
-        res += chooseOptimalDistance(
-            t - distances[i],
-            k - 1,
-            distances.slice(i + 1)
-        );
-
-        optimal = Math.max(res, optimal);
+function combine(k, ls, start, itemArray, sumArr) {
+    if (k === 0) {
+        sumArr.push(itemArray.reduce( (a, b) => a + b));
+        return;
     }
 
-    return optimal;
-};
+    for (let i = start; i <= ls.length - k; i++) {
+        itemArray[itemArray.length - k] = ls[i];
+        combine(k - 1, ls, i + 1, itemArray, sumArr);
+    }
+}
 
-console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61])); //173
+function chooseOptimalDistance(t, k, ls) {
+    const sumArr = [];
+    const itemArray = [];
+    itemArray.length = k;
+
+    combine(k, ls, 0, itemArray, sumArr);
+
+    let res = sumArr.sort((a, b) => b - a).filter(item => item <= t).shift();
+
+    return res ? res : null
+}
+
+
+console.log(chooseOptimalDistance(173, 3, [51, 56, 58, 59, 61])); //173
 console.log(chooseOptimalDistance(163, 3, [50])); // null
+console.log(chooseOptimalDistance(2430, 15, [2333, 144, 132, 123, 123, 100, 89, 89, 76, 73, 68, 64, 56, 56, 50, 44, 34])); //1287
